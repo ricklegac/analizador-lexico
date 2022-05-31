cdf3aa265922bb33098837735ed4c32a69dec10c
ricklegac <rick.legac@gmail.com>
2022-05-31 13:18:57 -0400

    declaracion de tokens

--- lexxxer.py
+++ lexxxer.py
@@ -1,1 +1,12 @@
 import ply.lex as lex 
+import re 
+import codecs 
+import os 
+import sys
+
+tokens = ['ID','NUMBER','PLUS','MINUS','TIMES','DIVIDE','ODD',
+'ASSIGN','NE','LT','LTE','GT','GTE','LPARENT','RPARENT','COMMA','SEMMICOLOM',
+'DOT','UPDATE']
+"""
+definimos tokens en una lista []
+"""
